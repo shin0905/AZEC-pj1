74263d3949c23de4dcf5d74ad801bb69c080e0b8
shin <shin@M2-Macbook-Air.local>
2024-10-14 20:13:16 +0900

    index.jsx update for TODO page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,12 +3,62 @@ import { StyleSheet, Text, View } from 'react-native';
 const Index = () => {
     return (
 
-        <View>
-            <Text>
-                Hello World
-            </Text>
+        <View style={styles.container}>
+            <View>
+                <View>
+                    <Text>TODO app</Text>
+                    <Text>LogOut</Text>
+                </View>
+            </View>
+
+            <View>
+                <View>
+                    <Text style={styles.text}>未完了のTODO</Text>
+                    <Text>15Oct2024 10:23</Text>
+                </View>
+            </View>
+            <View>
+                <View>
+                    <Text>Delete</Text>
+                </View>
+            </View>
+        
+            <View>
+                <View>
+                    <Text>未完了のTODO</Text>
+                    <Text>12Oct2024 10:23</Text>
+                </View>
+            </View>
+            <View>
+                <View>
+                    <Text>Delete</Text>
+                </View>
+            </View>
+            <View>
+                    <Text>X</Text>
+            </View>
         </View>
+        
     )
 }
+
+const styles = StyleSheet.create({ 
+    container: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    text: {
+        color: '#ffffff',
+        backgroundColor: 'blue',
+        fontSize: 20,
+        fontWeight: 'bold',
+        padding: 16
+    }
+})
+
+
+
 
 export default Index
