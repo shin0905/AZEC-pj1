fb467a9a8c28b3b3de31cac4eba2cee9f242004c
shin <shin@M2-Macbook-Air.local>
2024-10-17 05:04:52 +0900

    Styling for Buttons

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
 
 const Index = () => {
     return (
@@ -13,39 +13,53 @@ const Index = () => {
 
             <View>
                 <View style={styles.todoListItem}>
-                    <Text style={styles.todoListItemTitle}>未完了のTODO</Text>
-                    <Text style={styles.todoListItemDate}>15Oct2024 10:23</Text>
+                    <View >
+                        <Text style={styles.todoListItemTitle}>未完了のTODO-1</Text>
+                        <Text style={styles.todoListItemDate}>15Oct2024 10:23</Text>
+                    </View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
                 </View>
             </View>
+   
             <View>
-                <View>
-                    <Text>X</Text>
+                <View style={styles.todoListItem}>
+                    <View >
+                        <Text style={styles.todoListItemTitle}>未完了のTODO-2</Text>
+                        <Text style={styles.todoListItemDate}>15Oct2024 10:23</Text>
+                    </View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
                 </View>
             </View>
+   
+            
+                <View style={styles.todoListItem}>
+                    <View >
+                        <Text style={styles.todoListItemTitle}>未完了のTODO-3</Text>
+                        <Text style={styles.todoListItemDate}>15Oct2024 10:23</Text>
+                    </View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
+                </View>
+           
         
-            <View>
-                <View>
-                    <Text>未完了のTODO</Text>
-                    <Text>12Oct2024 10:23</Text>
-                </View>
+            <View style = {styles.circleButton}>
+                <Text style = {styles.circleButtonLabel}>+</Text>
             </View>
-            <View>
-                <View>
-                    <Text>X</Text>
-                </View>
-            </View>
-            <View>
-                    <Text>+</Text>
-            </View>
+                    
         </View>
-        
+
     )
 }
 
 const styles = StyleSheet.create({ 
     container: {
         flex: 1,
-        backgroundColor: '#ffffff',
+        backgroundColor: '#ffffff'
     },
     header: {
         backgroundColor: '#79a8a9',
@@ -87,7 +101,29 @@ const styles = StyleSheet.create({
         fontSize: 12,
         lineHeight: 18,
         color: 'brack'
-    }
+    },
+    circleButton:{
+        width: 64,
+        height: 64,
+        borderRadius: 32,
+        backgroundColor: '#467FD3',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        right: 40,
+        bottom: 40,
+        shadowColor: '#000000',
+        shadowOffset: {width: 0, height: 8},
+        shadowOpacity: 0.75,
+        shadowRadius: 8
+
+    },
+    circleButtonLabel: {
+        color: '#ffffff',
+        fontSize: 40,
+        lineHeight: 48,
+        textAlign: 'center'
+    }   
 
 })
 
